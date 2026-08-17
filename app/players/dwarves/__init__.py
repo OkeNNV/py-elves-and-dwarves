64ba1e9402f0894a9a8796caf252cc77525748fa
Artem D <OkeNNV@gmail.com>
2026-08-17 15:32:31 +0300

    module names fix

--- app/players/dwarves/__init__.py
+++ app/players/dwarves/__init__.py
@@ -1,5 +1,5 @@
 from app.players.dwarves.dwarf import Dwarf
-from app.players.dwarves.dwarfblacksmith import DwarfBlacksmith
-from app.players.dwarves.dwarfwarrior import DwarfWarrior
+from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
+from app.players.dwarves.dwarf_warrior import DwarfWarrior
 
 __all__ = ["Dwarf", "DwarfBlacksmith", "DwarfWarrior"]
--- tests/test_main.py
+++ tests/test_main.py
@@ -7,8 +7,8 @@ import pytest
 from app import main
 from app.main import calculate_team_total_rating, elves_concert, feast_of_the_dwarves
 from app.players.dwarves.dwarf import Dwarf
-from app.players.dwarves.dwarfblacksmith import DwarfBlacksmith
-from app.players.dwarves.dwarfwarrior import DwarfWarrior
+from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
+from app.players.dwarves.dwarf_warrior import DwarfWarrior
 from app.players.elves.druid import Druid
 from app.players.elves.elf import Elf
 from app.players.elves.ranger import ElfRanger
